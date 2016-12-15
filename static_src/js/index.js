29256bd7074540bb599f1fc86477329d2714d3c6
Sergey <s-kobets617@yandex.ru>
2016-12-16 00:05:29 +0300

    add method checkValidity

--- static_src/js/index.js
+++ static_src/js/index.js
@@ -1,1 +1,27 @@
 require('../css/index.styl')
+
+window.addEventListener('DOMContentLoaded', function () {
+	const name = document.getElementById('name');
+	const email = document.getElementById('email');
+	const required = document.querySelectorAll('.js-form [required]');
+	const errorClass = document.querySelectorAll('.js-error');
+	const length = required.length;
+	const submit = document.querySelectorAll('.js-submit')[0];
+
+	let error = [];
+	submit.addEventListener('click', function (e) {
+		e.preventDefault();
+		const form = this.closest('form');
+		for (let i = 0; i < length; i += 1) {
+			if (!required[i].checkValidity()) {
+				error.push(required[i].getAttribute('type'));
+			}
+		}
+		errorClass.innerHTML = `Введите ${error.join(' ')}`;
+		console.log(errorClass, errorClass.innerHTML);
+	});
+
+	function falidate (nameS) {
+		nameS.checkValidity();
+	}
+});
