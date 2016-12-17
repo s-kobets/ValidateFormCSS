9d432de415bd90b820293a4950f65d7f98e69ab4
Sergey <s-kobets617@yandex.ru>
2016-12-17 23:59:35 +0300

    asdasdasd

--- static_src/js/index.js
+++ static_src/js/index.js
@@ -4,7 +4,7 @@ window.addEventListener('DOMContentLoaded', function () {
 	const name = document.getElementById('name');
 	const email = document.getElementById('email');
 	const required = document.querySelectorAll('.js-form [required]');
-	const errorClass = document.querySelectorAll('.js-error');
+	const errorClass = document.querySelectorAll('.js-error')[0];
 	const length = required.length;
 	const submit = document.querySelectorAll('.js-submit')[0];
 
@@ -17,8 +17,16 @@ window.addEventListener('DOMContentLoaded', function () {
 				error.push(required[i].getAttribute('type'));
 			}
 		}
-		errorClass.innerHTML = `Введите ${error.join(' ')}`;
-		console.log(errorClass, errorClass.innerHTML);
+		errorClass.innerHTML = '';
+
+		if (error.length > 0) {
+			errorClass.innerHTML = `Введите ${error.join(' ')}`;
+			error = [];
+		} else {
+			errorClass.classList.remove('error');
+			errorClass.classList.add('success');
+			errorClass.innerHTML = 'Данные введены верно'
+		}
 	});
 
 	function falidate (nameS) {
